8d252ceb187cb3067d7a1b2b51a10203a344c3b2
Jiawei Li <lijiawei2003@gmail.com>
2020-10-18 15:28:40 -0700

    Test code for learning in Java
    Create limelight, print degree and hello world

---
+++ random.java
@@ -0,0 +1,9 @@
+public class random{
+    public static void main(String[] args){
+        Limelight limelight = new Limelight();
+        limelight.gettx();
+        System.out.println(limelight.gettx());
+
+        System.out.println("Hello World");
+    }
+}
